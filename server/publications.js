0390d17d26eb6bca28ca49e861205735a67f2188
Lane <mail_zlj@163.com>
2015-04-11 16:25:59 +0800

    分页这一章结束了

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,14 @@
-Meteor.publish('posts', function(author) {
-  //return Posts.find({author: author});
-  return Posts.find();
+Meteor.publish('posts', function(options) {
+  check(options, {
+    sort: Object,
+    limit: Number
+  });
+  return Posts.find({}, options);
+});
+
+Meteor.publish('singlePost', function(id) {
+  check(id, String)
+  return Posts.find(id);
 });
 
 Meteor.publish('comments', function(postId) {
